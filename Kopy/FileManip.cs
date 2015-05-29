70577b8696863d943a410a4e35c112464c9e738f
shoerner <shoerner+git@shoerner.net>
2015-05-29 08:11:55 -0500

    Cleaned up usings, added file manipulation methods.

--- Kopy/FileManip.cs
+++ Kopy/FileManip.cs
@@ -1,15 +1,18 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Security.Cryptography;
 using System.IO;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace Kopy
 {
     class FileManip
     {
+        /// <summary>
+        /// Calls file checksum evaluation
+        /// </summary>
+        /// <param name="sumFile">Object representing file to be processed</param>
+        /// <param name="sumLevel">Sum to be made. Valid options: 1 (SHA1), 5 (MD5), 256 (SHA256), 512 (SHA512)</param>
+        /// <param name="checkSum">Output checksum</param>
         public void FileSum(FileObj sumFile, uint sumLevel, out String checkSum)
         {
             // initialize variable
@@ -48,15 +51,78 @@ namespace Kopy
                 }
             }
         }
-        public void PerformCopyBasic(FileObj inputFile, bool overwrite, String copyTo, out FileObj outputFile)
+        public void PerformCopySimple(FileObj inputFile, bool overwrite, String copyTo, out FileObj outputFile)
         {
             String newFile = copyTo + "\\" + inputFile.Name;
             File.Copy(inputFile.NamedPath, newFile, overwrite);
             outputFile = new FileObj(inputFile.Name, copyTo);
         }
+        /// <summary>
+        /// Expensive process used to copy each byte of the file individually. Each call of readbyte is allocating a new mini array.
+        /// </summary>
+        /// <param name="inputFile"></param>
+        /// <param name="overwrite"></param>
+        /// <param name="copyTo"></param>
+        /// <param name="attemptCount"></param>
+        /// <param name="ignoreBadDisk"></param>
+        /// <param name="verifyFile"></param>
+        /// <param name="outputFile"></param>
         public void PerformCopyByte(FileObj inputFile, bool overwrite, String copyTo, int attemptCount, bool ignoreBadDisk, bool verifyFile, out FileObj outputFile)
         {
+            String newFile = copyTo + "\\" + inputFile.Name;
+            FileMode mode;
+            using(FileStream inputStream = new FileStream(inputFile.NamedPath, FileMode.Open, FileAccess.Read, FileShare.Read))
+            {
+                // Attempt to read for 30 seconds. Throw exception after that.
+                inputStream.ReadTimeout = 30000;
+                // Note this will overwrite a destination file 
+                // TODO: Insert logic to ask about overwrite at destination
+                if(overwrite)
+                {
+                    mode = FileMode.Create;
+                }
+                else if (!overwrite && File.Exists(newFile))
+                {
+                    LocalLog.Instance.MakeEvent("File Exists on byte level copy, overwrite not set", "FileManip:PerformCopyByte", "WARNING");
+                    throw new IOException("FileExists");
+                }
+                else
+                {
+                    mode = FileMode.CreateNew;
+                }
+                using(FileStream outputStream = new FileStream(newFile, mode, FileAccess.ReadWrite, FileShare.None))
+                {
+                    for(int readByte = 0; readByte < inputStream.Length; readByte++)
+                    {
+                        byte[] bytes = new byte[1];
+                        int output = inputStream.Read(bytes, readByte, readByte + 1);
+                        if (output == 0)
+                            break;
+                        outputStream.WriteByte(bytes[0]);
+                    }
+                }
+            }
 
+            outputFile = new FileObj(inputFile.Name, copyTo);
+        }
+        public bool PerformMoveSimple(FileObj inputFile, bool overwrite, String destinationPath)
+        {
+            try
+            {
+                File.Copy(inputFile.NamedPath, (destinationPath + "\\" + inputFile.Name), overwrite);
+                inputFile.Path = destinationPath;
+                return true;
+            }
+            catch (IOException ioe)
+            {
+                LocalLog.Instance.MakeEvent("IOException caught: " + ioe.Message, "FileManip:PerformMoveSimple", "ERROR");
+                return false;
+            }
+            catch(Exception e)
+            {
+                LocalLog.Instance.MakeEvent("Exception caught on file move: " + e.StackTrace, "FileManip:PerformMoveSimple", "ERROR");
+                return false;
+            }
         }
     }
 }
